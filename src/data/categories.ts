5c1300af02dc3b98b44c4d4d74c301a799facb55
Apostolis Dimitriou <aposdimit@gmail.com>
2025-06-16 20:35:33 +0300

    Added new cat

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -4,4 +4,8 @@ export const categories = [
     file: "enotita_1_sintagmatiko_dikaio.yaml",
   },
   { label: "Ενότητα 2 - Διοικητικό", file: "enotita_2_dioikitiko_dikaio.yaml" },
+  {
+    label: "Ενότητα 3 - Ευρωπαϊκοί θεσμοί και δίκαιο",
+    file: "enotita_3_europaikoi_thesmoi_kai_dikaio.yaml",
+  },
 ];
