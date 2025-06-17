9e183cfd1be7156d138f2c4ac5e299cc43bd8b16
Apostolis Dimitriou <aposdimit@gmail.com>
2025-06-17 22:03:40 +0300

    added new cats

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -12,4 +12,16 @@ export const categories = [
     label: "Ενότητα 4 - Οικονομικές Επιστήμες",
     file: "enotita_4_oikonomikes_epistimes.yaml",
   },
+  {
+    label: "Ενότητα 5 - Δημόσια Διοίκηση",
+    file: "enotita_5_dimosia_dioikisi.yaml",
+  },
+  {
+    label: "Ενότητα 6 - Πληροφορική",
+    file: "enotita_6_pliroforiki.yaml",
+  },
+  {
+    label: "Ενότητα 7 - Ιστορία",
+    file: "enotita_7_istoria.yaml",
+  },
 ];
